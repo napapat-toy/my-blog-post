eb085cb6a057e65dfaacda895c45e0a13a96579d
toytoy08 <toy31327@gmail.com>
2024-09-12 05:00:35 +0700

    comment button for deploy

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "./ui/button";
+// import { Button } from "./ui/button";
 import Link from "next/link";
 
 const Navbar = () => {
