2e0407a8f88f0299e35375125312b5b52be2a142
toytoy08 <toy31327@gmail.com>
2024-09-12 05:35:55 +0700

    fixed for deploy

--- app/(root)/post/[id]/page.tsx
+++ app/(root)/post/[id]/page.tsx
@@ -2,16 +2,16 @@ import { getPostById } from "@/lib/post.action";
 import React from "react";
 
 const PostPage = async ({ params: { id } }: { params: { id: string } }) => {
-  const { title, username, description } = await getPostById(id);
+  const post = await getPostById(id);
 
   return (
     <div className="w-full h-screen flex items-center justify-center drop-shadow-xl">
       <div className="w-[600px] h-fit bg-white rounded-md p-6">
         <div className="h-full w-full flex flex-col ">
-          <h2 className="font-semibold text-3xl">{title}</h2>
-          <p className="text-xl mt-6">{description}</p>
+          <h2 className="font-semibold text-3xl">{post?.title}</h2>
+          <p className="text-xl mt-6">{post?.description}</p>
           <p className="text-slate-500 self-end justify-self-end">
-            By {username}
+            By {post?.username}
           </p>
         </div>
       </div>
